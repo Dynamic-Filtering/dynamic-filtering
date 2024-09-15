1081591911d92e4f01fbe7fd8f9b62629c81462e
Job Haast <jobhaast@hotmail.com>
2024-09-15 15:24:19 +0200

    TSDoc for select options

--- projects/dynamic-filtering/src/lib/models/filtering/options/multi-select-option.model.ts
+++ projects/dynamic-filtering/src/lib/models/filtering/options/multi-select-option.model.ts
@@ -1,8 +1,25 @@
 import { SelectOption } from './select-option.model';
 
+/**
+ * Represents an option in a multi-select filter, extending the basic select option with a selection state.
+ *
+ * @template T - The type of the value associated with the option (e.g., string, number).
+ * @extends SelectOption<T>
+ */
 export class MultiSelectOption<T> extends SelectOption<T> {
+    /**
+     * Indicates whether the option is selected.
+     * Defaults to `false` if not specified.
+     */
     selected: boolean = false;
 
+    /**
+     * Creates an instance of `MultiSelectOption`.
+     *
+     * @param label - The display label for the option, typically shown to the user.
+     * @param value - The value associated with the option, typically used in the application's logic.
+     * @param selected - Optional boolean indicating whether the option is initially selected. Defaults to `false`.
+     */
     constructor(label: string, value: T, selected?: boolean) {
         super(label, value);
 
--- projects/dynamic-filtering/src/lib/models/filtering/options/select-option.model.ts
+++ projects/dynamic-filtering/src/lib/models/filtering/options/select-option.model.ts
@@ -1,7 +1,25 @@
+/**
+ * Represents an option in a select input component, typically used in single-select filters.
+ *
+ * @template T - The type of the value associated with the option (e.g., string, number).
+ */
 export class SelectOption<T> {
+    /**
+     * The display label of the option.
+     */
     label: string;
+
+    /**
+     * The value associated with the option.
+     */
     value: T;
 
+    /**
+     * Creates an instance of `SelectOption`.
+     *
+     * @param label - The display label for the option, typically shown to the user.
+     * @param value - The value associated with the option, typically used in the application's logic.
+     */
     constructor(label: string, value: T) {
         this.label = label;
         this.value = value;
